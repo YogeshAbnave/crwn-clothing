3ce3a55b4b8c11eee0a560ac7d679bad3ccf7a84
YogeshAbnave <yogeshabnave101@gmail.com>
2022-03-07 18:46:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePage from './homePage.Cmponent';
+function App() {
+  return (
+    <div>
+ <HomePage/>
+ <h1>hello worlds</h1>
+    </div>
+  );
+}
+
+export default App;
